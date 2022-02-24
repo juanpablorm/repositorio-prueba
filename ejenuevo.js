5ba5ce0e9d13baf185d9684dd96da9a0e81636a4
Juan Pablo Rios Martinez <juanpa-emdm@hotmail.com>
2022-02-23 19:48:44 -0600

    Second commit

---
+++ ejenuevo.js
@@ -0,0 +1,19 @@
+// Create a function that, given an array with numbers: [1,1,2,2,3,3,1,1,1,2,3]
+//Return an array without consecutive duplicates: [1,2,3,1,2,3]
+
+const miArray = [1,1,2,2,3,3,1,1,1,2,3];
+
+console.log(miArray);
+
+const acomodo = () =>{
+
+for (let i = miArray.length - 1; i >= 0; --i){
+    if (miArray[i] === miArray[i - 1]){
+        miArray.splice(i, 1);
+    }
+}
+console.log(miArray);
+
+}
+
+acomodo();
