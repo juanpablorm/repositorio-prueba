c226f8d673be77d55b71b7e9c1b129013b2b7726
Juan Pablo Rios Martinez <juanpa-emdm@hotmail.com>
2022-02-23 19:34:48 -0600

    First commit

---
+++ ejercicio2.js
@@ -0,0 +1,20 @@
+// Create a function that, given an array with numbers: [1,1,2,2,3,3,1,1,1,2,3]
+//Return an array without consecutive duplicates: [1,2,3,1,2,3]
+
+const miArray = [1,1,2,2,3,3,1,1,1,2,3];
+
+console.log(miArray);
+
+
+const acomodo = () =>{
+
+for (let i = 0; i <= miArray.length - 1; ++i){
+    if (miArray[i] === miArray[i + 1] || miArray[i] === miArray[i - 1]){
+        miArray.splice(i, 1);
+    }
+}
+console.log(miArray);
+
+}
+
+acomodo();
