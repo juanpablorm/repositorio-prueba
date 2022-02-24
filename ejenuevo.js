19df11c6c6b4151d8db6c0b0982fe2c2f3929e8a
Juan Pablo Rios Martinez <juanpa-emdm@hotmail.com>
2022-02-24 10:28:47 -0600

    Third commit

--- ejenuevo.js
+++ ejenuevo.js
@@ -1,7 +1,7 @@
 // Create a function that, given an array with numbers: [1,1,2,2,3,3,1,1,1,2,3]
 //Return an array without consecutive duplicates: [1,2,3,1,2,3]
 
-const miArray = [1,1,2,2,3,3,1,1,1,2,3];
+const miArray = [1,1,2,2,3,3,1,1,1,2,3,3,1,1,2,3];
 
 console.log(miArray);
 
